734ca869187c5e9a62489fd6a639e73ea6203aeb
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-04-25 21:08:46 +0300

    add TodoList

--- src/App.js
+++ src/App.js
@@ -24,34 +24,65 @@
 
 // export default App;
 
-import Painting from './components/Painting'
-import paintings from './paintings.json';
+// import Painting from './components/Painting'
+// import paintings from './paintings.json';
 
-export default function App() {
-    return <div>
-    <Painting
-        url={paintings[0].url}
-        title={paintings[0].title} 
-        author={paintings[0].author.tag}
-        profileUrl={paintings[0].author.url}
-        price={paintings[0].price}
-        quantity={paintings[0].quantity} 
-        />
-    <Painting
-        url={paintings[1].url}
-        title={paintings[1].title} 
-        author={paintings[1].author.tag}
-        profileUrl={paintings[1].author.url}
-        price={paintings[1].price}
-        quantity={paintings[1].quantity} 
-        />
-        <Painting
-        url={paintings[2].url}
-        title={paintings[2].title} 
-        author={paintings[2].author.tag}
-        profileUrl={paintings[2].author.url}
-        price={paintings[2].price}
-        quantity={paintings[2].quantity} 
-        />
-    </div>;
+import TodoList from './components/TodoList';
+import { Component } from 'react';
+
+// export default function App() {
+//     return <div>
+//     <Painting
+//         url={paintings[0].url}
+//         title={paintings[0].title} 
+//         author={paintings[0].author.tag}
+//         profileUrl={paintings[0].author.url}
+//         price={paintings[0].price}
+//         quantity={paintings[0].quantity} 
+//         />
+//     <Painting
+//         url={paintings[1].url}
+//         title={paintings[1].title} 
+//         author={paintings[1].author.tag}
+//         profileUrl={paintings[1].author.url}
+//         price={paintings[1].price}
+//         quantity={paintings[1].quantity} 
+//         />
+//         <Painting
+//         url={paintings[2].url}
+//         title={paintings[2].title} 
+//         author={paintings[2].author.tag}
+//         profileUrl={paintings[2].author.url}
+//         price={paintings[2].price}
+//         quantity={paintings[2].quantity} 
+//         />
+//     </div>;
+// }
+export default class App extends Component {
+    state = {
+        todos: [{id: 'id-1', text: 'Вивчити основи React', completed: false },
+        {id: 'id-2', text: 'Розібратись з React Router', completed: false },
+        {id: 'id-3', text: 'Пережити Redux', completed: false },
+        ],
+    };
+
+    deleteTodo = (todoId) => {
+        this.setState(prevState => ({
+            todos: prevState.todos.filter(todo => todo.id !== todoId),
+        } ))
+    }
+
+    render() {
+        const { todos } = this.state;
+        return (
+            <>
+            <h1>Стан компонента</h1>
+    
+            <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        </> 
+        );
+    }
 }
+
+
+
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './TodoList.css';
+
+const TodoList = ({todos, onDeleteTodo}) => (
+<ul className="TodoList">
+    {todos.map(({id, text}) => (
+        <li key={id} className="TodoList_item">
+        <p className="TodoList_text">{text}</p>
+        <button onClick={() => onDeleteTodo(id)}>Delete</button>
+        </li>
+    ))}
+</ul>
+);
+
+export default TodoList;
